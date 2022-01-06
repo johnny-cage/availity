2a00806676015971d7baab77933e80a4d0dbdd44
Wan M. Wimberly <arwm4@Wans-Air.attlocal.net>
2022-01-05 20:19:49 -0500

    Remove console logs

--- Program.cs
+++ Program.cs
@@ -23,12 +23,6 @@ using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
         var sortedEnrollees = enrolleeMap.Values.OrderBy(e => e.LastName).OrderBy(e => e.FirstName);
         var outputFileName = string.Format("{0}-enrollees.csv", company.Key);
 
-        foreach(var e in sortedEnrollees) {
-                     Console.WriteLine(
-                "First Name: {0}, Last Name: {1} Version: {2} Company: {3} UserId : {4}",
-                e.FirstName, e.LastName, e.Version, e.Company, e.UserId
-            );
-        }
         using (var writer = new StreamWriter(outputFileName))
         using (var outputCsv = new CsvWriter(writer, CultureInfo.InvariantCulture))
         {
